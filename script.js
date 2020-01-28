bf15d8be68267f29b9bd8a65dbc40c2f785e9d22
Gutierrez <brandongutierrez@my.whccd.edu>
2020-01-27 21:25:13 -0800

    Not finished, but I get it.

---
+++ script.js
@@ -0,0 +1,23 @@
+//IF ELSE 
+let myGrade = 65
+
+if (myGrade >=90) {
+    console.log("You got an A! Congrats!");
+} 
+else if(myGrade < 90 && myGrade > 80){
+    console.log("You got a B. You can do better!");
+}
+else if (myGrade < 80 && myGrade > 70) {
+    console.log("You got a C. Not the worst!");
+}
+else if (myGrade < 70 && myGrade > 60) {
+    console.log("You got a D. Got to start trying!");
+}
+else if (myGrade < 60 && myGrade > 50) {
+    console.log("You got a F. Oh man.");
+}
+else {
+    console.log("An error has occurred.")
+}
+
+//FOR loop
